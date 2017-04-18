f7405a9875dfd5bf1e0c7a28fe6734dd00a6d062
Don Henton <donhenton@gmail.com>
2017-04-18 16:09:04 -0500

    comics 5

--- front-end/react/components/comicPanel.js
+++ front-end/react/components/comicPanel.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Component } from 'react';
 import ReactDOM  from 'react-dom';
-
+import moment from 'moment';
 
 const COMIC_IMAGE_REF = 'comicImageRef';
- 
+
 
 export default class ComicPanel extends Component {
 
@@ -21,7 +21,7 @@ export default class ComicPanel extends Component {
     }
     componentDidMount()
     {
-         
+
         let imageRef = ReactDOM.findDOMNode(this.refs[COMIC_IMAGE_REF]);
         let img = new Image();
         let newSrc = this.state.comicData.thumbnail;
@@ -32,20 +32,30 @@ export default class ComicPanel extends Component {
             $(imageRef).attr('src', newSrc);
         }
         img.src = newSrc;
- 
-    }
 
-     
+    }
+    
+    getDate()
+    {
+        //"2008-10-29T00:00:00-0400",
+        let mm = moment(this.state.comicData.date,"YYYY-MM-DD");
+        return  mm.format("MM/DD/YYYY")
+    }
 
     render()
     {
 
         return (
                 <div className='comic-panel grouping'>
-                   <img ref={COMIC_IMAGE_REF} src="css/imgs/xlarge_na.jpg" /> 
-                     comic panel   
+                    <div className='comic-title'>{this.state.comicData.title}</div>
+                    <img ref={COMIC_IMAGE_REF} src="css/imgs/xlarge_na.jpg" /> 
                 
-                   
+                    <div className='comic-info'>
+                    
+                    <span className='comic-price'>$ {this.state.comicData.price.price}</span>
+                    <span className='comic-date'>{this.getDate()}</span>
+                    </div>
+                
                 </div>
 
                 )
