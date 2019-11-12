95c49b0f9c661aae7076cd6ae29483e957a201f3
Don Henton <donhenton@gmail.com>
2019-11-12 14:05:08 -0600

    removing mongoose using in memory session starting on docker file

--- config/env/production.js
+++ config/env/production.js
@@ -1,18 +1,8 @@
 // Invoke 'strict' JavaScript mode
 'use strict';
 
-// this is the deviant art client id and secret 
-// the dev copy is not saved in git and must be recreated 
 
-/*
-module.exports = {
-	 client_id: deviant id,
-         client_secret: deviant secret,
-         deviantStorageDB:  'mongodb://localhost/deviantart',
-         sessionSecret: "somesession secret"
-};
-*/
-
+ 
 
 module.exports =  {
      publicKey: process.env.publicKey,
--- config/express.js
+++ config/express.js
@@ -3,105 +3,97 @@
 
 'use strict';
 var codeUtils = new require('./../app/utils/utils')();
-
+var session = require('express-session');
+var MemoryStore = require('memorystore')(session)
 
 // Load the module dependencies
 var config = require('./config'),
-        express = require('express'),
-        morgan = require('morgan'),
-        compress = require('compression'),
-        bodyParser = require('body-parser'),
-        methodOverride = require('method-override'), 
-        session = require('express-session');
-        
+      express = require('express'),
+      morgan = require('morgan'),
+      compress = require('compression'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      session = require('express-session');
+
 var fs = require('fs');
 var vm = require('vm');
 var cookieParser = require('cookie-parser');
-var passport = require('passport')  ;
+
 
 // Define the Express configuration method
 module.exports = function () {
-    // Create a new Express application instance
-    var app = express();
+  // Create a new Express application instance
+  var app = express();
 
-    // Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
-    if (process.env.NODE_ENV === 'development') {
-        app.use(morgan('dev'));
-    } else if (process.env.NODE_ENV === 'production') {
-        app.use(compress());
-    }
-    var cookieOpts = {};
-    app.use(cookieParser(config.sessionSecret,cookieOpts));
-    
-    // see https://www.npmjs.com/package/cookie for the various options
-    // that can be specificed in the cookieOpts;
+  // Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+  var cookieOpts = {};
+  app.use(cookieParser(config.sessionSecret, cookieOpts));
 
-    // Use the 'body-parser' and 'method-override' middleware functions
-    var mongoose = require('mongoose');
-// Connect to DB
-    mongoose.connect(config.deviantStorageDB); 
-    
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
-    
-    
-     var MongoStore = require('connect-mongo')(session);
-    app.use(session({
-        secret: config.sessionSecret,
-        resave: true,
-        saveUninitialized: true,        
-        store: new MongoStore({
-            url: config.deviantStorageDB
-        })
-    }));
-    
-    
-    
+  // see https://www.npmjs.com/package/cookie for the various options
+  // that can be specificed in the cookieOpts;
+
+  // Use the 'body-parser' and 'method-override' middleware functions
  
+ 
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+
+
+  app.use(session({
+    secret: config.sessionSecret,
+    resave: true,
+    saveUninitialized: true,
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+     
+  }));
+
    
-   /* set up security 
  
-   var initPassport =  require('../passport/initPassport');
-   initPassport(passport);
-    app.use(passport.initialize());
-    app.use(passport.session());
-    */
-    
-    // Set the application view engine and 'views' folder
-    app.set('views', './app/views');
-    app.set('view engine', 'ejs');
 
-    // Load the 'index' routing file
+  // Set the application view engine and 'views' folder
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
+  // Load the 'index' routing file
   /*  
-    
-    var deviantStoreService = require('../app/services/deviantStoreService.js')(config);
-    
    
-    require('../app/routes/rest.routes.js')(app,deviantArtService); 
-    require('../app/routes/deviantStore.routes.js')(app,deviantStoreService);
-    require('../app/routes/passport.routes.js')(app,passport);
-*/
-    var marvelService = require('../app/services/marvelService.js')(config);
-     require('../app/routes/main.routes.js')(app); 
-     require('../app/routes/rest.routes.js')(app,marvelService); 
+   var deviantStoreService = require('../app/services/deviantStoreService.js')(config);
+   
+   
+   require('../app/routes/rest.routes.js')(app,deviantArtService); 
+   require('../app/routes/deviantStore.routes.js')(app,deviantStoreService);
+   require('../app/routes/passport.routes.js')(app,passport);
+   */
+  var marvelService = require('../app/services/marvelService.js')(config);
+  require('../app/routes/main.routes.js')(app);
+  require('../app/routes/rest.routes.js')(app, marvelService);
 
-    // Configure static file serving
-    app.use(express.static('./public_html'));
-    
-    /* error handlers must be located at end */
-    var clientErrorProcessor =  require('../app/filters/clientErrorProcessor');
-    var generalErrorProcessor =  require('../app/filters/generalErrorProcessor');
-    app.use(clientErrorProcessor);
-    app.use(generalErrorProcessor);
+  // Configure static file serving
+  app.use(express.static('./public_html'));
 
-    // Return the Express application instance
-    
-    //always place this as the last this is the 404 handler
-     require('../app/routes/not.found.routes.js')(app);
-    
-    
-    return app;
+  /* error handlers must be located at end */
+  var clientErrorProcessor = require('../app/filters/clientErrorProcessor');
+  var generalErrorProcessor = require('../app/filters/generalErrorProcessor');
+  app.use(clientErrorProcessor);
+  app.use(generalErrorProcessor);
+
+  // Return the Express application instance
+
+  //always place this as the last this is the 404 handler
+  require('../app/routes/not.found.routes.js')(app);
+
+
+  return app;
 };
